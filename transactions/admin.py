9a9ff2f288b2b140678750eed33ba398fc8cbc2a
m3rc1k <maksimkakcmblcl@gmail.com>
2025-04-21 21:37:14 +0300

    transactions admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction
+
+
+@admin.register(Transaction)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'transaction_type', 'amount', 'date' )
+
