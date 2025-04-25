34a780f8fd5367dab9dfd88f24e695eb5ded3dc6
m3rc1k <maksimkakcmblcl@gmail.com>
2025-04-25 21:09:18 +0300

    personal best balance, better/worse difference, info for current month

--- account/models.py
+++ account/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Sum
 from django.urls import reverse
 
 
@@ -14,6 +17,73 @@ class User(AbstractUser):
         db_table = 'user'
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+
+    def get_monthly_income(self, month, year):
+        return self.user_transactions.filter(
+            transaction_type = 'Income',
+            date__month = month,
+            date__year = year
+        ).aggregate(total=Sum('amount'))['total'] or 0
+
+    def get_monthly_expense(self, month, year):
+        return self.user_transactions.filter(
+            transaction_type = 'Expense',
+            date__month = month,
+            date__year = year
+        ).aggregate(total=Sum('amount'))['total'] or 0
+
+    def get_personal_best(self):
+        transactions = self.user_transactions.all().order_by('date')
+
+        current_balance = 0
+        max_balance = 0
+
+        for transaction in transactions:
+            if transaction.transaction_type == 'Income':
+                current_balance += transaction.amount
+            else:
+                current_balance -= transaction.amount
+            if current_balance > max_balance:
+                max_balance = current_balance
+
+        return max_balance
+
+
+    def get_monthly_comparison(self):
+        current_date = datetime.now()
+        current_month = current_date.month
+        current_year = current_date.year
+
+        current_income = self.get_monthly_income(current_month, current_year)
+        current_expense = self.get_monthly_expense(current_month, current_year)
+        current_difference = current_income - current_expense
+
+        prev_month_date = current_date - timedelta(days=current_date.day)
+        prev_month = prev_month_date.month
+        prev_year = prev_month_date.year
+
+        prev_income = self.get_monthly_income(prev_month, prev_year)
+        prev_expense = self.get_monthly_expense(prev_month, prev_year)
+        prev_difference = prev_income - prev_expense
+
+        if prev_difference != 0:
+            percent_diff = ((current_difference - prev_difference) / abs(prev_difference)) * 100
+        else:
+            percent_diff = 0
+
+        if current_difference > prev_difference:
+            comparison = 'better'
+        elif current_difference < prev_difference:
+            comparison = 'worse'
+        else:
+            comparison = 'same'
+
+        return {
+            'percent_diff': abs(round(percent_diff)),
+            'comparison': comparison,
+        }
+
 
     def __str__(self):
         return self.username
--- main/views.py
+++ main/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from datetime import datetime
+from calendar import month_name
+
+from transactions.models import Transaction
+
 
 def welcome(request):
     return render(request, 'main/welcome.html', {})
 
 def dashboard(request):
-    return render(request, 'main/dashboard.html', {})
+    user = request.user
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    income = user.get_monthly_income(current_month, current_year)
+    expense = user.get_monthly_expense(current_month, current_year)
+    transactions = Transaction.objects.filter(user = user)[:6]
+    max_balance = user.get_personal_best()
+    month_comparison = user.get_monthly_comparison()
+
+    return render(request, 'main/dashboard.html', {
+        'current_month_income': income,
+        'current_month_expenses': expense,
+        'current_month_difference': income - expense,
+        'current_month': month_name[current_month],
+        'transactions': transactions,
+        'max_balance': max_balance,
+        'month_comparison': month_comparison,})
